0601ac5a84ad1cdd6d43d261d4c41335b3591978
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-20 12:47:49 +0900

    For

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Метод 1
+void Copyright()
+{
+    Console.WriteLine("All rights reserved");
+}
+
+Copyright();
+
+
+// Метод 2
+void Method2 (string message)
+{
+    Console.WriteLinne(message);
+}
+Method2 ("Text");
+Method2 (message: "text"); // явно названные аргументы, если их нескольк, порядок уже не важен
+
+// Метод 3
+
+int Method3 ()
+{
+    return DateTime.Now.Year;
+}
+
+Method3 ();
+
