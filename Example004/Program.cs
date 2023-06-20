afea76a39cdddb8983f246d0ad6cfcadf0a0dbdf
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-20 14:10:54 +0900

    Array sorting max

--- Example004/Program.cs
+++ Example004/Program.cs
@@ -1,4 +1,44 @@
-﻿// Упорядочить массив от минимума к максимуму 
+﻿// // Упорядочить массив от минимума к максимуму 
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         // Console.WriteLine(array[i]);
+//         Console.Write($"{array[i]} ");
+
+//     }
+//     Console.WriteLine();
+// }
+
+// void SelctionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         // 
+//         // 
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+
+
+// }
+
+// PrintArray(arr);
+// SelctionSort(arr);
+// PrintArray(arr);
+
+// Упорядочить массив от максимума к минимуму 
 
 int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
@@ -14,26 +54,26 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-void SelctionSort(int[] array)
+void SelctionSortMax(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
     {
-        int minPosition = i;
+        int maxPosition = i;
 
         for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[maxPosition]) maxPosition = j;
         }
         // 
         // 
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
     }
 
 
 }
 
 PrintArray(arr);
-SelctionSort(arr);
+SelctionSortMax(arr);
 PrintArray(arr);
