483bb8114740842067cf09c4e536d81bc989fb04
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-20 14:05:56 +0900

    Array sorting

---
+++ Example004/Program.cs
@@ -0,0 +1,39 @@
+﻿// Упорядочить массив от минимума к максимуму 
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        // Console.WriteLine(array[i]);
+        Console.Write($"{array[i]} ");
+
+    }
+    Console.WriteLine();
+}
+
+void SelctionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        // 
+        // 
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+
+
+}
+
+PrintArray(arr);
+SelctionSort(arr);
+PrintArray(arr);
